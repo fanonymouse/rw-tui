221c6dd8d774b5f95e30fde8017198c4aa258a0b
Fan Yin <fan.yin@live.com>
2025-04-30 11:49:32 +0800

    bug fix. pci bus number displayed incorrectly.

--- src/utils/hex_string.cpp
+++ src/utils/hex_string.cpp
@@ -1,13 +1,15 @@
 #include "utils/hex_string.hpp"
 
-string hexString(uint64_t value, uint64_t digits, bool ellipsis) {
+string hexString(uint64_t value, uint64_t digits, bool ellipsis, bool withPrefix) {
   std::stringstream stream;
-  stream << setfill('0') << setw(digits) << hex << uppercase << value;
+  if (withPrefix) {
+    stream << "0x";
+  }
+  stream << std::setfill('0') << std::setw(digits) << std::hex << std::uppercase << value;
   std::string valueStr(stream.str());
   if (ellipsis == true) {
     uint64_t valueLength = valueStr.length();
-    valueStr =
-        valueStr.substr(0, 2) + ".." + valueStr.substr(valueLength - 4, 4);
+    valueStr = valueStr.substr(0, withPrefix ? 4 : 2) + ".." + valueStr.substr(valueLength - 4, 4);
   }
   return valueStr;
 }
